666f7e3dbb306fa9deb2d60bd4fac1cfef12644c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-30 14:59:59 +0100

    Created bin/test_basic_classification.py

---
+++ bin/test_basic_classification.py
@@ -0,0 +1,17 @@
+"""Test basic_classification file
+"""
+from basic_classification import *
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-30"
+
+def test():
+	"""Tests the basic_classification function in the basic_classification class
+	Write here all test you want to do.
+	REMEMBER to test your programm you can't use __init__ function
+	"""
+	assert basic_classification.basic_classification() == "basic_classification", "test failed"
+	#assert basic_classification.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
+	
+if __name__ == "__main__":
+	test()
