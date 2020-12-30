050af6eb63939bfb8ce8da7909ae70d9c1bb350f
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-30 14:59:53 +0100

    Created bin/basic_classification.py

---
+++ bin/basic_classification.py
@@ -0,0 +1,19 @@
+"""basic_classification
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-30"
+
+class basic_classification:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(basic_classification.basic_classification())
+	
+	def basic_classification():
+		"""basic_classification first funtion
+		"""
+		return "basic_classification" #for the test (see test_basic_classification.py)
+		
+if __name__ == "__main__":
+	basic_classification()
