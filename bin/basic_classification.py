2b5fd2a3e2282482ea134fb533ea40e38fc55940
castellanidavide <davidecastellani@castellanidavide.it>
2020-12-31 10:36:40 +0100

    Basic classification

--- bin/basic_classification.py
+++ bin/basic_classification.py
@@ -1,5 +1,14 @@
 """basic_classification
 """
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt
+
+print(tf.__version__)
 
 __author__ = "help@castellanidavide.it"
 __version__ = "1.0 2020-12-30"
@@ -8,12 +17,80 @@ class basic_classification:
 	def __init__ (self):
 		"""Where it all begins
 		"""
-		print(basic_classification.basic_classification())
+		self.load_model()
+		self.print_some_model_infos()
+		#self.show_img()
+		self.create_the_model()
+		self.feed_model()
+		self.check_accuracy()
 	
-	def basic_classification():
-		"""basic_classification first funtion
+	def load_model(self):
+		"""Load the model
 		"""
-		return "basic_classification" #for the test (see test_basic_classification.py)
+		self.fashion_mnist = tf.keras.datasets.fashion_mnist
+
+		(self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.fashion_mnist.load_data()
+		self.train_images = self.train_images / 255.0
+		self.test_images = self.test_images / 255.0
 		
+		self.class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+	def print_some_model_infos(self):
+		"""Prints some infos from the model
+		"""
+		print(f"Shape: {self.train_images.shape}")
+		print(f"Len: {len(self.train_labels)}")
+		print(f"Labels: {self.train_labels}")
+		print(f"Test shape: {self.test_images.shape}")
+		print(f"Test len: {len(self.test_labels)}")
+
+	def show_img(self):
+		"""Preprocess the data
+		"""
+		# Only one
+		"""plt.figure()
+		plt.imshow(self.train_images[0])
+		plt.colorbar()
+		plt.grid(False)
+		plt.show()"""
+
+		# Grind
+		plt.figure(figsize=(10,10))
+		n_photos = 30
+		for i in range(n_photos):
+			plt.subplot(int(sqrt(n_photos)), n_photos / int(sqrt(n_photos)), i + 1)
+			plt.xticks([])
+			plt.yticks([])
+			plt.grid(False)
+			plt.imshow(self.train_images[i])
+			plt.xlabel(f"{i}) {self.class_names[self.train_labels[i]]}")
+		plt.show()
+
+	def create_the_model(self):
+		"""Creates the module
+		"""
+		self.model = tf.keras.Sequential([
+			tf.keras.layers.Flatten(input_shape=(28, 28)),
+			tf.keras.layers.Dense(128, activation='relu'),
+			tf.keras.layers.Dense(10)
+		])
+
+		self.model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+	def feed_model(self):
+		"""Fit my personal model
+		"""
+		self.model.fit(self.train_images, self.train_labels, epochs=10)
+
+	def check_accuracy(self):
+		"""Now checks the accuracy
+		"""
+		test_loss, test_acc = self.model.evaluate(self.test_images, self.test_labels, verbose=2)
+		
+		print(f"Test accuracy: {test_acc}")
+
 if __name__ == "__main__":
 	basic_classification()
--- bin/test_basic_classification.py
+++ bin/test_basic_classification.py
@@ -1,6 +1,6 @@
 """Test basic_classification file
 """
-from basic_classification import *
+from basic_classification import basic_classification
 
 __author__ = "help@castellanidavide.it"
 __version__ = "1.0 2020-12-30"
@@ -10,7 +10,7 @@ def test():
 	Write here all test you want to do.
 	REMEMBER to test your programm you can't use __init__ function
 	"""
-	assert basic_classification.basic_classification() == "basic_classification", "test failed"
+	assert basic_classification() != "", "test failed"
 	#assert basic_classification.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
 	
 if __name__ == "__main__":
